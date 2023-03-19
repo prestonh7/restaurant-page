efb200bb2ffe1094d9d92173b19bbbaa46917798
prestonh7 <prestonhouse7@gmail.com>
2023-03-19 14:45:14 -0500

    Devtools, server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
     module: {
     rules: [
