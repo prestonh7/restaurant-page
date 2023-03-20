540cb48c43b2dbaf0f04359eadc934e855c9bfcd
prestonh7 <prestonhouse7@gmail.com>
2023-03-20 15:36:45 -0500

    Style, filler text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('working!');
+import "./style.css";
+
+const content = document.querySelector('.content');
+
+// const sidePanel = (() => { 
+//     const container = document.querySelector('.container');
+//     const panelImage = new Image();
+//     panelImage.src = sideImage;
+//     panelImage.style = 'position: relative';
+//     container.appendChild(panelImage);
+// })();
