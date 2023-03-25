1a6ae299f14400be6513d7dcd6454b13af48a6d3
prestonh7 <prestonhouse7@gmail.com>
2023-03-24 19:18:01 -0500

    Constructors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import "./style.css";
 
-const content = document.querySelector('.content');
-const header = document.querySelector('header');
-const headerHeight = getComputedStyle(header).getPropertyValue('height');
-document.documentElement.style.setProperty('--header-height', headerHeight);
+const heightAdjust = (() => { 
+    const header = document.querySelector('header');
+    const headerHeight = getComputedStyle(header).getPropertyValue('height');
+    document.documentElement.style.setProperty('--header-height', headerHeight);
+})();
+
+const homeScreen = (() => {
+    const content = document.querySelector('.content');
+    function drawContent() {
+        
+    }
+    return { drawContent }
+});
+
+const displayController = (() => {
+    const content = document.querySelector('.content');
+    function updateContent(page) {
+        clearContent();
+        if (page === 'home') {
+            homeScreen.drawContent();
+        } else if (page === 'products') {
+
+        } else if (page === 'contactUs') {
+
+        }
+    }
+    function clearContent() {
+        content.innerText = '';
+    }
+    return { updateContent }
+});
