fb460727307ef57b959b52788afff26ef063c728
prestonh7 <prestonhouse7@gmail.com>
2023-03-25 14:10:35 -0500

    draw content

--- src/index.js
+++ src/index.js
@@ -8,14 +8,27 @@ const heightAdjust = (() => {
 
 const homeScreen = (() => {
     const content = document.querySelector('.content');
+
     function drawContent() {
-        
+        const para = document.createElement('p');
+        para.textContent = "Welcome to Whisk Me Away Bakery, where every bite is a sweet escape! \
+        Our expert bakers use only the finest ingredients to create delicious treats \
+        that will transport you to a world of flavor and indulgence. \
+        From our famous croissants to our decadent cakes, \
+        everything we make is crafted with care and precision. \
+        Browse our products to find your next sweet escape - \
+        you can access them from the header or click here to see our full range of treats."
+        content.appendChild(para);
     }
+
     return { drawContent }
 });
 
 const displayController = (() => {
     const content = document.querySelector('.content');
+    const homeBtn  = document.getElementById('homeBtn');
+    homeBtn.addEventListener('click', updateContent('home'));
+    
     function updateContent(page) {
         clearContent();
         if (page === 'home') {
@@ -26,8 +39,10 @@ const displayController = (() => {
 
         }
     }
+
     function clearContent() {
         content.innerText = '';
     }
+
     return { updateContent }
 });
